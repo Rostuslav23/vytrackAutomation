6fad3e05d9fa6b7cb056b82dfa6d3eb37206161b
hulya <hulyakeles94@gmail.com>
2022-03-05 13:04:41 -0600

    US7 first commit

--- src/test/java/com/vytrack/tests/US7.java
+++ src/test/java/com/vytrack/tests/US7.java
@@ -1,4 +1,24 @@
 package com.vytrack.tests;
 
-public class US7 {
+import com.vytrack.tests.base.TestBase;
+import com.vytrack.utilities.VytrackUtils;
+import org.testng.annotations.Test;
+
+public class US7 extends TestBase {
+    //testing module for sales manager
+
+    @Test
+    public void as_sales_manager(){
+        VytrackUtils.loginAsSalesManager();
+    }
+
+    @Test
+    public void as_store_manager(){
+        VytrackUtils.loginAsStoreManger();
+    }
+
+    @Test
+    public void as_driver(){
+        VytrackUtils.loginAsDriver();
+    }
 }
