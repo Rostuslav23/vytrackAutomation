41e43c109c40fbd31a9804f71b8ac81f582fdd2e
hulya <hulyakeles94@gmail.com>
2022-03-05 18:02:49 -0600

    US7-TC1

--- src/test/java/com/vytrack/tests/US7.java
+++ src/test/java/com/vytrack/tests/US7.java
@@ -5,7 +5,12 @@ import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.Driver;
 import com.vytrack.utilities.VytrackUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class US7 extends TestBase {
     //testing checkboxes for only store manager, as truck manager and sales manager checkboxes have bugs
@@ -15,7 +20,14 @@ public class US7 extends TestBase {
         //login to the page as a Store Manager
         VytrackUtils.loginAsStoreManger();
         BrowserUtils.sleep(5);
-        Driver.getDriver().findElement(By.xpath("//a[@href='entity/Extend_Entity_Carreservation']"));
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"))).perform();
+        actions.doubleClick(Driver.getDriver().findElement(By.xpath("//a[@href='entity/Extend_Entity_Carreservation']"))).perform();
+
+        BrowserUtils.sleep(5);
+
+        List<WebElement> allCheckBoxes = Driver.getDriver().findElements(By.xpath("//input[@data-role='select-row-cell']"));
+        allCheckBoxes.forEach(p-> Assert.assertTrue(!(p.isSelected())));
         BrowserUtils.sleep(5);
     }
     @Test
