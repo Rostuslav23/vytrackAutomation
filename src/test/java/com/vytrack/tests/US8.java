0820ff01d52dd5f1e1d62ca24c5aee654602d1dc
merto <mertozer384@gmail.com>
2022-03-09 11:20:05 -0500

    user story us8 getting readyyyyyyy

--- src/test/java/com/vytrack/tests/US8.java
+++ src/test/java/com/vytrack/tests/US8.java
@@ -14,7 +14,7 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class US8 {
+public class US8 extends TestBase {
 
     @DataProvider(name = "TruckDriverLogin")
     public Object[][] truckDriver(){
