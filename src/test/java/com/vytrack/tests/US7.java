c56a5a1246293394411cbadae68783b9984a9f25
hulya <hulyakeles94@gmail.com>
2022-03-10 17:26:50 -0600

    updated US7

--- src/test/java/com/vytrack/tests/US7.java
+++ src/test/java/com/vytrack/tests/US7.java
@@ -23,10 +23,8 @@ public class US7 extends TestBase {
     @Test(dataProvider = "StoreManagersLogin", priority = 1)
     public void store_manager_view_unchecked_checkboxes(String storeManagerUsername){
         VytrackUtils.login(storeManagerUsername,ConfigurationReader.getProperty("password"));
+        VytrackUtils.waitTillLoaderMaskDisappear();
         SupToSub.goToPage("Fleet","Vehicles");
-//        fleet_vehicle(storeManagerUsername);
-
-        //finding all checkboxes
         List<WebElement> allCheckBoxes = Driver.getDriver().findElements(By.xpath("//input[@data-role='select-row-cell']"));
         //verify all of them are unchecked by using !isSelected() method
         allCheckBoxes.forEach(p-> Assert.assertTrue(!(p.isSelected())));
@@ -35,9 +33,8 @@ public class US7 extends TestBase {
     @Test(dataProvider = "StoreManagersLogin",priority = 2)
     public void store_managers_click_first_checkbox_to_check_all_the_cars(String storeManagerUsername){
         VytrackUtils.login(storeManagerUsername,ConfigurationReader.getProperty("password"));
+        VytrackUtils.waitTillLoaderMaskDisappear();
         SupToSub.goToPage("Fleet","Vehicles");
-
-//        fleet_vehicle(storeManagerUsername);
         List<WebElement> allCheckBoxes = Driver.getDriver().findElements(By.xpath("//input[@data-role='select-row-cell']"));
         Driver.getDriver().findElement(By.xpath("(//div[@class='btn-group dropdown']//input)[1]")).click();
         BrowserUtils.sleep(2);
@@ -47,17 +44,14 @@ public class US7 extends TestBase {
     @Test(dataProvider = "StoreManagersLogin",priority = 3)
     public void store_managers_check_any_cars_checkbox(String storeManagerUsername){
         VytrackUtils.login(storeManagerUsername,ConfigurationReader.getProperty("password"));
+        VytrackUtils.waitTillLoaderMaskDisappear();
         SupToSub.goToPage("Fleet","Vehicles");
-//       fleet_vehicle(storeManagerUsername);
-        //finding all checkboxes
         List<WebElement> allCheckBoxes = Driver.getDriver().findElements(By.xpath("//input[@data-role='select-row-cell']"));
         //making sure all checkboxes are NOT selected
         allCheckBoxes.forEach(p-> Assert.assertTrue(!(p.isSelected())));
         //clicking each check box then make sure it is selected
-        for (WebElement eachCheckBox : allCheckBoxes) {
-            eachCheckBox.click();
-            Assert.assertTrue(eachCheckBox.isSelected());
-        }
+        allCheckBoxes.forEach(p->{p.click(); Assert.assertTrue(p.isSelected());});
+
     }
 }
 
